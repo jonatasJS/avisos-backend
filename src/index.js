3f7c0541a0b7fc2c94b71fc1198cf20dad5e2d37
jonatasJS <soaresjonatas398@gmail.com>
2022-09-29 16:34:32 -0300

    \zx\zx\zx

--- src/index.js
+++ src/index.js
@@ -15,7 +15,14 @@ mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
 });
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "*",
+    "https://avisos-nextjs.vercel.app",
+    "https://avisos.jonatas.app"
+  ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
