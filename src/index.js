235fc70e4141b9d7893d184ba7c213e6c90e9569
jonatasjs <soaresjonatas398@gmail.com>
2022-10-26 22:02:21 -0300

    asdasd

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ require("dotenv").config();
 console.log(process.env.PORT || 3000);
 
 const express = require("express");
+const https = require("https");
+const fs = require("fs");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const socket = require("socket.io");
 const path = require("path");
 const cors = require("cors");
 
+// ssl certificate
+// ca_bundle.crt
+// certificater.crt
+// const options = {
+//   ca: fs.readFileSync("ca_bundle.crt", {
+//     encoding: "utf-8",
+//   }),
+//   cert: fs.readFileSync("certificate.crt", {
+//     encoding: "utf-8",
+//   }),
+//   key: fs.readFileSync("private.key", {
+//     encoding: "utf-8",
+//   }), 
+// };
+
 const expressApp = express();
-const app = require('http').createServer(expressApp);
+const app = require('https').createServer(/* options,*/ expressApp);
 const io = socket(app);
 
 
